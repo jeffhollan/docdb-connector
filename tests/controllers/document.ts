9130452258205a4314df86581a2fea4cf7aaa1e0
jeffhollan <jeffhollan@outlook.com>
2016-08-01 22:44:16 -0700

    added test for controller-docs GET

--- tests/controllers/document.ts
+++ tests/controllers/document.ts
@@ -5,165 +5,204 @@ import * as strings from "../../resources/strings";
 import * as resources from "../resources";
 import * as nock from "nock";
 
-const configureNock = function () {
-    nock('https://test.documents.azure.com')
-      .post(/docs$/)
-      .reply(201, resources.sample_create_doc_response);
-}
-
 describe('controllers', function () {
     describe('document', function () {
-        describe('post', function () {
-            
-            describe('validation', function () {
-                it('should not accept any path parameters', function (done) {
-                    request(app.server)
-                        .post('/docs/1234')
-                        .expect(404);
-                    done();
-                });
-
-                it('should accept a trailing slash', function (done) {
-                    request(app.server)
-                        .post('/docs/')
-                        .expect(400)
-                        .end(function (err, res) {
-                            done();
-                        });
-
-                });
-
-                it('should require masterkey', function (done) {
-                    process.env.masterkey = '';
-                    request(app.server)
-                        .post('/docs')
-                        .expect(400)
-                        .end(function (err, res) {
-                            should.not.exist(err);
-                            res.body.should.equal(strings.err_missing_master_key);
-                            done();
-                        });
-
-                });
-
-                it('should require x-ms-dbs header', function (done) {
-
-                    request(app.server)
-                        .post('/docs')
-                        .set('x-ms-masterkey', '1234567890')
-                        .expect(400)
-                        .end(function (err, res) {
-                            should.not.exist(err);
-                            res.body.should.equal(strings.err_missing_dbs_header);
-                            done();
-                        });
-
-                });
-
-                it('should require x-ms-colls header', function (done) {
-                    process.env.masterkey = '';
-                    request(app.server)
-                        .post('/docs')
-                        .set('x-ms-masterkey', '1234567890')
-                        .set('x-ms-dbs', 'testingDB')
-                        .expect(400)
-                        .end(function (err, res) {
-                            should.not.exist(err);
-                            res.body.should.equal(strings.err_missing_colls_header);
-                            done();
-                        });
-
-                });
-
-                it('should require x-ms-account header', function (done) {
-                    process.env.masterkey = '';
-                    request(app.server)
-                        .post('/docs')
-                        .set('x-ms-masterkey', '1234567890')
-                        .set('x-ms-dbs', 'testingDB')
-                        .set('x-ms-colls', 'testingColls')
-                        .expect(400)
-                        .end(function (err, res) {
-                            should.not.exist(err);
-                            res.body.should.equal(strings.err_missing_account_header);
-                            done();
-                        });
-
-                });
-
-                it('should require valid JSON Body', function (done) {
-
-                    configureNock();
-                    this.slow(1500);
-                    let test_string = "some string";
-                    process.env.masterkey = '';
-                    request(app.server)
-                        .post('/docs')
-                        .set('x-ms-masterkey', '1234567890')
-                        .set('x-ms-dbs', 'testingDB')
-                        .set('x-ms-colls', 'testingColls')
-                        .set('x-ms-account', 'test')
-                        .send(test_string)
-                        .expect(400)
-                        .end(function (err, res) {
-                            should.not.exist(err);
-                            res.body.should.equal(strings.err_invalid_json_body(test_string));
-                            test_array_request;
-                        });
-
-                    let test_array = ["some", "array"];
-                    const test_array_request = request(app.server)
-                        .post('/docs')
-                        .set('x-ms-masterkey', '1234567890')
-                        .set('x-ms-dbs', 'testingDB')
-                        .set('x-ms-colls', 'testingColls')
-                        .set('x-ms-account', 'test')
-                        .send(test_array)
-                        .expect(400)
-                        .end(function (err, res) {
-                            should.not.exist(err);
-                            res.body.should.equal(strings.err_invalid_json_body(test_array));
-                            // done();
-                            test_object_request;
-                        });
-
-                    let test_valid_object = { "id": "test-valid-object" };
-                    const test_object_request = request(app.server)
-                        .post('/docs')
-                        .set('x-ms-masterkey', '1234567890')
-                        .set('x-ms-dbs', 'testingDB')
-                        .set('x-ms-colls', 'testingColls')
-                        .set('x-ms-account', 'test')
-                        .send(test_valid_object)
-                        .expect(201)
-                        .end(function (err, res) {
-                            should.not.exist(err);
-                            done();
-                        });
-                });
+        describe('validation', function () {
+            it('should not accept any path parameters', function (done) {
+                request(app.server)
+                    .post('/docs/1234')
+                    .expect(404);
+                done();
             });
 
-            describe('create doc', function () {
+            it('should accept a trailing slash', function (done) {
+                request(app.server)
+                    .post('/docs/')
+                    .expect(400)
+                    .end(function (err, res) {
+                        done();
+                    });
+
+            });
+
+            it('should require masterkey', function (done) {
+                request(app.server)
+                    .post('/docs')
+                    .expect(400)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        res.body.should.equal(strings.err_missing_master_key);
+                        done();
+                    });
+
+            });
+
+            it('should require x-ms-dbs header', function (done) {
+
+                request(app.server)
+                    .post('/docs')
+                    .set('x-ms-masterkey', '1234567890')
+                    .expect(400)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        res.body.should.equal(strings.err_missing_dbs_header);
+                        done();
+                    });
+
+            });
+
+            it('should require x-ms-colls header', function (done) {
+                request(app.server)
+                    .post('/docs')
+                    .set('x-ms-masterkey', '1234567890')
+                    .set('x-ms-dbs', 'testingDB')
+                    .expect(400)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        res.body.should.equal(strings.err_missing_colls_header);
+                        done();
+                    });
+
+            });
+
+            it('should require x-ms-account header', function (done) {
+                request(app.server)
+                    .post('/docs')
+                    .set('x-ms-masterkey', '1234567890')
+                    .set('x-ms-dbs', 'testingDB')
+                    .set('x-ms-colls', 'testingColls')
+                    .expect(400)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        res.body.should.equal(strings.err_missing_account_header);
+                        done();
+                    });
+
+            });
+        });
+        describe('post', function () {
+            it('should require valid JSON Body', function (done) {
 
                 configureNock();
+                this.slow(1500);
+                let test_string = "some string";
+                request(app.server)
+                    .post('/docs')
+                    .set('x-ms-masterkey', '1234567890')
+                    .set('x-ms-dbs', 'testingDB')
+                    .set('x-ms-colls', 'testingColls')
+                    .set('x-ms-account', 'test')
+                    .send(test_string)
+                    .expect(400)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        res.body.should.equal(strings.err_invalid_json_body(test_string));
+                        test_array_request;
+                    });
+
+                let test_array = ["some", "array"];
+                const test_array_request = request(app.server)
+                    .post('/docs')
+                    .set('x-ms-masterkey', '1234567890')
+                    .set('x-ms-dbs', 'testingDB')
+                    .set('x-ms-colls', 'testingColls')
+                    .set('x-ms-account', 'test')
+                    .send(test_array)
+                    .expect(400)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        res.body.should.equal(strings.err_invalid_json_body(test_array));
+                        // done();
+                        test_object_request;
+                    });
+
+                let test_valid_object = { "id": "test-valid-object" };
+                const test_object_request = request(app.server)
+                    .post('/docs')
+                    .set('x-ms-masterkey', '1234567890')
+                    .set('x-ms-dbs', 'testingDB')
+                    .set('x-ms-colls', 'testingColls')
+                    .set('x-ms-account', 'test')
+                    .send(test_valid_object)
+                    .expect(201)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        done();
+                    });
+            });
+            it('should return 201', function (done) {
+                configureNock();
                 let test_input = { "id": "test-create-doc" }
-                it('should return 201', function (done) {
-                    request(app.server)
-                        .post('/docs')
-                        .set('x-ms-masterkey', 'testing-create')
-                        .set('x-ms-dbs', 'testingDB')
-                        .set('x-ms-colls', 'testingColls')
-                        .set('x-ms-account', 'test')
-                        .send(test_input)
-                        .expect(201)
-                        .end(function (err, res) {
-                            should.not.exist(err);
-                            res.body.id.should.equal('AndersenFamily');
-                            done();
-                        });
-                })
+                request(app.server)
+                    .post('/docs')
+                    .set('x-ms-masterkey', 'testing-create')
+                    .set('x-ms-dbs', 'testingDB')
+                    .set('x-ms-colls', 'testingColls')
+                    .set('x-ms-account', 'test')
+                    .send(test_input)
+                    .expect(201)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        res.body.id.should.equal('AndersenFamily');
+                        done();
+                    });
+            });
+        });
+        describe('get', function () {
+            it('should not allow multiple paths', function(done){
+                request(app.server)
+                    .get('/docs/12345/abc')
+                    .set('x-ms-masterkey', '1234567890')
+                    .set('x-ms-dbs', 'testingDB')
+                    .set('x-ms-colls', 'testingColls')
+                    .set('x-ms-account', 'test')
+                    .expect(404)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        done();
+                    });
+            });
+
+            it('should require id path', function(done){
+                request(app.server)
+                    .get('/docs/')
+                    .set('x-ms-masterkey', '1234567890')
+                    .set('x-ms-dbs', 'testingDB')
+                    .set('x-ms-colls', 'testingColls')
+                    .set('x-ms-account', 'test')
+                    .expect(405)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        done();
+                    });
+            });
+
+            it('should return 200', function(done){
+                configureNock();
+                request(app.server)
+                    .get('/docs/test')
+                    .set('x-ms-masterkey', '1234567890')
+                    .set('x-ms-dbs', 'testingDB')
+                    .set('x-ms-colls', 'testingColls')
+                    .set('x-ms-account', 'test')
+                    .expect(200)
+                    .end(function (err, res) {
+                        should.not.exist(err);
+                        res.body.id.should.equal('SalesOrder1');
+                        done();
+                    });
             });
         });
     });
 });
 
+
+function configureNock() {
+    nock('https://test.documents.azure.com')
+        .post(/docs$/)
+        .reply(201, resources.sample_create_doc_response);
+    
+    nock('https://test.documents.azure.com')
+        .get(/docs\/test$/)
+        .reply(200, resources.sample_get_doc_response);
+}
--- tests/resources.ts
+++ tests/resources.ts
@@ -36,3 +36,28 @@ export const sample_create_doc_response : Object = {
   "_ts": 1459218509,
   "_attachments": "attachments/"
 }
+
+export const sample_get_doc_response : Object = {
+  "id": "SalesOrder1",
+  "ponumber": "PO18009186470",
+  "OrderDate": "2005-07-01T00:00:00",
+  "ShippedDate": "0001-01-01T00:00:00",
+  "AccountNumber": "Account1",
+  "SubTotal": 419.4589,
+  "TaxAmount": 12.5838,
+  "Freight": 472.3108,
+  "TotalDue": 985.018,
+  "Items": [
+    {
+      "OrderQty": 1,
+      "ProductId": 760,
+      "UnitPrice": 419.4589,
+      "LineTotal": 419.4589
+    }
+  ],
+  "_rid": "d9RzAJRFKgwBAAAAAAAAAA==",
+  "_self": "dbs/d9RzAA==/colls/d9RzAJRFKgw=/docs/d9RzAJRFKgwBAAAAAAAAAA==/",
+  "_etag": "\"0000d986-0000-0000-0000-56f9e25b0000\"",
+  "_ts": 1459216987,
+  "_attachments": "attachments/"
+}
